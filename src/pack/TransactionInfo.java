cbda26da92910c18db949a9c6a20db6ee7b6da57
HUANGJianshui <58004791+HUANGJianshui@users.noreply.github.com>
2021-06-01 23:00:26 +0200

    Add files via upload

--- src/pack/TransactionInfo.java
+++ src/pack/TransactionInfo.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class TransactionInfo {
@@ -11,6 +12,58 @@ public class TransactionInfo {
 	@GeneratedValue(strategy=
 					GenerationType.IDENTITY)
 	private int id;
+	
+	private int purchaserId;
+	private int sellerId;
+	private int price;
+	
+	@OneToOne
+	private User purchaser;
+	
+	@OneToOne 
+	private User seller;
+	
+
+	public int getPurchaserId() {
+		return purchaserId;
+	}
+
+	public void setPurchaserId(int purchaserId) {
+		this.purchaserId = purchaserId;
+	}
+
+	public int getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(int sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public User getPurchaser() {
+		return purchaser;
+	}
+
+	public void setPurchaser(User purchaser) {
+		this.purchaser = purchaser;
+	}
+
+	public User getSeller() {
+		return seller;
+	}
+
+	public void setSeller(User seller) {
+		this.seller = seller;
+	}
+	
 
 	public int getId() {
 		return id;
